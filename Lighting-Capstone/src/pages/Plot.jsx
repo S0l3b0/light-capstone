5a725cd0298d524c15d16dc44f4d3a858b64ec67
corvidbonez <fbillie@icloud.com>
2026-03-23 10:20:42 -0700

    fix of Lit component

--- Lighting-Capstone/src/pages/Plot.jsx
+++ Lighting-Capstone/src/pages/Plot.jsx
@@ -1,9 +1,10 @@
+import Lit from "/src/components/infoLit.jsx";
 function Plot() {
     return (
     <div className="h-screen w-screen">
       <h1>Plot Page</h1>
       
-      <div className ='text-center'>
+      <div className ='text-justify'>
       <Lit
           category='Par'
           pic='https://cdn11.bigcommerce.com/s-cr8amw/images/stencil/1280x1280/products/1182/2913/S4_PAR__28368.1735673408.png?c=2'
