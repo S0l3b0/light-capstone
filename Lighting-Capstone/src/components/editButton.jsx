6374489e0343fe367d9d189044bad4ed21d4a4b6
corvidbonez <fbillie@icloud.com>
2026-03-25 09:41:17 -0700

    component to edit plot, bug fixes of pfps

---
+++ Lighting-Capstone/src/components/editButton.jsx
@@ -0,0 +1,9 @@
+function EditButton(){
+    return(
+        <div className ="m-3">
+        <a href="/src/pages/Sort.jsx"><button>Edit</button></a>
+    </div>
+ );
+};
+
+export default EditButton;
--- Lighting-Capstone/src/pages/Plot.jsx
+++ Lighting-Capstone/src/pages/Plot.jsx
@@ -1,10 +1,18 @@
-import Lit from "/src/components/infoLit.jsx";
+import EditButton from "/src/components/editButton.jsx";
+ 
 function Plot() {
     return (
+    <div class='grid'>
     <div className="h-screen w-screen">
       <h1>Plot Page</h1>
+      <span className = "grid grid-flow-col grid-rows-4 gap-0">
+
+      </span>
+      <div className = "justify-center">
+        <EditButton/>
+      </div>
     </div>
-      
+    </div>
   );
   }
   
