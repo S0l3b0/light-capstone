0ea5eb3f5d2c40735d45f309c4b6e1085e608a5d
Sophie Bowyer <sophiebowyer@TinaKoeberl.lan.ousd.org>
2026-04-15 09:46:31 -0700

    archive

--- Lighting-Capstone/src/components/archivePost.jsx
+++ Lighting-Capstone/src/components/archivePost.jsx
@@ -6,7 +6,7 @@ function ArchivePost({ show, image, name, date }) {
         <div className="bg-indigo-50 m-4 text-center border-2 border-neutral-800 w-100 h-100">
             <h2 className="text-indigo-950 text-xl font-bold text-center">{show}</h2>
             <br />
-            <img className="flex text-neutral-700 p-2" src={image}></img>
+            <img className="p-2 h-75 m-auto" src={image}></img>
             <p className="text-neutral-900 p-4 text-center">- {name}, {date}</p>
             <br/>
         </div>
--- Lighting-Capstone/src/pages/Plot.jsx
+++ Lighting-Capstone/src/pages/Plot.jsx
@@ -3,29 +3,29 @@ import Light_key from "../components/key";
 import S4 from "../components/s4";
 import Pa from "../components/par";
 import Fres from "../components/fres";
- 
+import Draggable from "react-draggable";
+
 function Plot() {
-    return (
+  return (
     <div class='gridbg'>
-    <div className="h-screen w-screen">
-      <h1 className = "text-indigo-50">Plot Page</h1>
-      <span className = "grid grid-cols-3 gap-0">
-        <S4/>
-        <Fres/>
-        <Pa/>
-        <div class='col-span-2'><Pa/></div>
-        <S4/>
-        <Fres/>
-      </span>
-      <Light_key/>
-      <div className = "justify-center">
-        <EditButton/>
+      <div className="h-screen w-screen">
+        <h1 className="text-indigo-50">Plot Page</h1>
+        <span className="grid grid-cols-3 gap-0">
+          <S4 />
+          <Fres />
+          <Pa />
+          <div class='col-span-2'><Pa /></div>
+          <S4 />
+          <Fres />
+        </span>
+        <Light_key />
+        <div className="justify-center">
+          <EditButton />
+        </div>
       </div>
     </div>
-    </div>
   );
-  }
-  
-  export default Plot;
-  
-  
+}
+
+export default Plot;
+
